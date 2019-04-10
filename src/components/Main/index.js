8c83eb0becd82573f1b8e21a00f946cc70309207
Andrei Sokolov <xlloll3@gmail.com>
2019-04-10 19:29:26 +0300

    стили переписаны на css modules

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,3 +1,2 @@
 import Main from './Main.js';
-import './style.css';
 export default Main;
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import styles from './Player.module.css';
 
 const Player = props => {
   return (
-    <div className="player_container">
-      <div style={{"height":"320px","weight":"320px"}}>
+    <div className={styles.PLayerContainer}>
+      <div style={{ "height": "320px", "weight": "320px" }}>
         Picture
       </div>
-      <div style={{"height": "32px"}}>
+      <div className={styles.PlayerContainer_Child} style={{ "height": "32px" }}>
         sound bar
       </div>
-      <div style={{'height' : '64px'}}>
-        btns 
+      <div className={styles.PlayerContainer_Child} style={{ 'height': '64px' }}>
+        btns
       </div>
     </div>
   );
