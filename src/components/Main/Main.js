ae73b722b2bd0a2ac5e11734178f140bcf3a7586
Maxim Skibitskiy <maksimka719@gmail.com>
2019-04-15 13:42:25 +0300

    Добавлен API

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,6 +8,16 @@ import Some from '../CentralBlocks/Some.js';
 import Greating from '../CentralBlocks/Greating.js';
 
 class Main extends React.Component {
+  componentDidMount() {
+    document.addEventListener('musickitloaded', () => {
+      this.music = this.MusicKit.configure({
+        developerToken: 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlhKWVI5TFpCNVgifQ.eyJpYXQiOjE1NDQwMjU5MzEsImV4cCI6MTU1OTU3NzkzMSwiaXNzIjoiVUM5REc5Mko2SiJ9.NE-pTkKDHS6klZ52oub617LWgvedHMYFG4-p8csfuIQH60S7gGwSIWeigY7h4R_eKcLA8X3KZqyMT0H0Ix73Iw',
+        name: 'Codepen',
+        build: '1'
+      }).getInstance();
+    });
+  }
+
   render () {
     return (
       <Router>
